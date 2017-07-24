02e85b68498188104ad68faca17df51891d103dd
benjaml <b.lefevre@rubika-edu.com>
2017-07-24 21:43:31 +0200

    first commit

---
+++ Source/AICompanion/Private/AICompanionBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "AICompanionBPLibrary.h"
+#include "AICompanion.h"
+
+UAICompanionBPLibrary::UAICompanionBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UAICompanionBPLibrary::AICompanionSampleFunction(float Param)
+{
+	return -1;
+}
+
